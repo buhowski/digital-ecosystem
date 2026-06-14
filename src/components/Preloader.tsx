5e4e2ff4f8d111fd4f629d9aa830e1e69b2475db
Olexander Tsiomakh <olexander.tsiomakh@gmail.com>
2026-06-14 22:12:18 +0300

    redesign preloader

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,11 +1,17 @@
+const totalSpans = 12;
+
 const Preloader = () => {
 	return (
-		<div className='preloader'>
+		<div className='preloader' style={{ '--preloader-spans': totalSpans } as React.CSSProperties}>
 			<div className='preloader__spinner'>
-				{Array(12)
+				{Array(totalSpans)
 					.fill(null)
 					.map((_, index) => (
-						<span className='preloader__spinner-span' key={index}></span>
+						<span
+							className='preloader__spinner-span'
+							key={index}
+							style={{ '--index': index } as React.CSSProperties}
+						></span>
 					))}
 			</div>
 		</div>
